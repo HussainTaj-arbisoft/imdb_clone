a0c8905e653017b35d9cd69cf5d0dfff58fb4e59
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-12-07 16:06:50 +0500

    Add backend config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,7 +44,11 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "djoser",
     # Custom
-    "accounts.apps.AccountsConfig",
+    "accounts",
+    "movies",
+    "celebrities",
+    # Dev
+    "dev_tools",
 ]
 
 MIDDLEWARE = [
@@ -126,7 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "../frontend/build/static"),
@@ -150,3 +155,8 @@ DJOSER = {
         "user": "accounts.serializers.UserCreateSerializer",
     },
 }
+
+
+# CUSTOM
+
+CELEBRITY_MODEL = "celebrities.Celebrity"
