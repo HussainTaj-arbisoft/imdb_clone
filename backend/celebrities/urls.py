cf058b09045833e17b20b368da13d300e9b72593
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-12-07 18:44:52 +0500

    Add CelebritiesBornTodayListAPIVIew

--- backend/celebrities/urls.py
+++ backend/celebrities/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import CelebritiesListAPIView
+from .views import CelebritiesListAPIView, CelebritiesBornTodayListAPIView
 
-urlpatterns = [path("", CelebritiesListAPIView.as_view())]
+urlpatterns = [
+    path("", CelebritiesListAPIView.as_view()),
+    path("born_today", CelebritiesBornTodayListAPIView.as_view()),
+]
--- backend/celebrities/views.py
+++ backend/celebrities/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import LimitOffsetPagination
+from django.utils import timezone
 
 from .serializers import CelebritySerializer
 from .models import Celebrity
@@ -8,4 +9,10 @@ from .models import Celebrity
 class CelebritiesListAPIView(ListAPIView):
     serializer_class = CelebritySerializer
     queryset = Celebrity.objects.all()
+    pagination_class = LimitOffsetPagination
+
+
+class CelebritiesBornTodayListAPIView(ListAPIView):
+    serializer_class = CelebritySerializer
+    queryset = Celebrity.objects.order_by("-debut_date")
     pagination_class = LimitOffsetPagination
