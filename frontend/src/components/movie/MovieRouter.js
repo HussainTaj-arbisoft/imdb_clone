e1ce8f3387c091656ceb2170fa8ce4cda72f1101
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-12-10 14:08:36 +0500

    Remove unnecessary div

--- frontend/src/components/movie/MovieRouter.js
+++ frontend/src/components/movie/MovieRouter.js
@@ -6,12 +6,10 @@ import MovieDetail from './MovieDetail';
 
 export default function MovieRouter(props) {
     return (
-        <div>
-            <Switch>
-                <Route exact path={`${props.match.path}`} component={Home} />
-                <Route path={`${props.match.path}movie/:id`} component={MovieDetail} />
-                <Redirect to={`${props.match.path}`} />
-            </Switch>
-        </div>
+        <Switch>
+            <Route exact path={`${props.match.path}`} component={Home} />
+            <Route exact path={`${props.match.path}movie/:id`} component={MovieDetail} />
+            <Redirect to={`${props.match.path}`} />
+        </Switch>
     )
 }
