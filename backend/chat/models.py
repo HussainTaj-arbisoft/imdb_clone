4645a1b239fff2a5cf07a504ffe373daed00fe64
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-12-11 21:30:31 +0500

    Add Message chat app model

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User,
+        verbose_name="Sender",
+        on_delete=models.DO_NOTHING,
+        related_name="sent_messages",
+    )
+    receiver = models.ForeignKey(
+        User,
+        verbose_name="Receiver",
+        on_delete=models.DO_NOTHING,
+        related_name="received_messages",
+    )
+    text = models.TextField("text")
+    time = models.DateTimeField("Time", auto_now=False, auto_now_add=True)
+
+    class Meta:
+        ordering = ("-time",)
