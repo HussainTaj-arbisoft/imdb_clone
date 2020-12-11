809e24a94ab84247deda8ed12fdc130dac95158c
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-12-11 21:29:44 +0500

    Add UserSerializer and ProfileSerializer

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,10 +1,30 @@
-from djoser.serializers import UserCreateSerializer, UserSerializer
+import djoser.serializers
 from rest_framework import serializers
-from .models import User
+from .models import User, Profile
 
 
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+
+class UserSerializer(djoser.serializers.UserSerializer):
+    profile = ProfileSerializer()
+
+    class Meta(djoser.serializers.UserSerializer.Meta):
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "last_seen",
+            "profile",
+        ]
+
+
+class UserCreateSerializer(djoser.serializers.UserCreateSerializer):
+    class Meta(djoser.serializers.UserCreateSerializer.Meta):
         model = User
         fields = (
             "id",
