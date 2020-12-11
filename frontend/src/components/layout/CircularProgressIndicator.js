d32f1bffa32beec1a96be4165fb0aa27f9e720f4
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-12-12 01:02:45 +0500

    Change font awesome spinner to SVG spinner

--- frontend/src/components/layout/CircularProgressIndicator.js
+++ frontend/src/components/layout/CircularProgressIndicator.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import { ReactComponent as AnimatedIcon } from './icon/CirularProgressIndicatorAnimatedIcon.svg'
 
 export default function CircularProgressIndicator(props) {
+    let height = props.height ?? "50px";
+    let width = props.width ?? "50px";
+    let bottomText = props.bottomText ? <p>{props.bottomText}</p> : null;
     return (
-        <div className={props.className}>
-            <div className="text-center">
-                <i
-                    className={`${props.sizeClass ?? "fa-3x"} fa fa-circle-o-notch fa-spin fa-fw text-primary`}
-                ></i>
-                <p>{props.bottomText}</p>
-            </div>
+        <div className={`${props.className} w-100 h-100 d-flex flex-column justify-content-center align-items-center`}>
+            <AnimatedIcon style={{ height, width }} className="fa-spin" />
+            {bottomText}
         </div>
     )
 }
