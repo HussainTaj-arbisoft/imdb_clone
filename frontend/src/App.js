f8a49f1a697ce5c44e7ffeff2bab8e3db9341861
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-12-13 20:21:05 +0500

    Remove react redux connection to auth prop.
    Functionality will be replaced by withAuth HOC and AuthenticateUser component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,13 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
 import React, { Component } from 'react'
 
 
 import './App.css';
 import AuthRouter from './components/auth/AuthRouter';
-import * as authActions from './store/actions/authActions'
-
 import MovieRouter from './components/movie/MovieRouter';
 import ChatRouter from './components/chat/ChatRouter';
 
 class App extends Component {
-  componentDidMount() {
-    if (!this.props.isAuthenticated) {
-      this.props.signInWithTokenCookieIfExists();
-    }
-  }
-
   render() {
     return (
       <div className="App">
@@ -32,14 +23,4 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    isAuthenticated: state.auth.isAuthenticated
-  }
-}
-
-const appActions = ({
-  signInWithTokenCookieIfExists: authActions.signInWithTokenCookieIfExists,
-});
-
-export default connect(mapStateToProps, appActions)(App);
+export default App;
