d9c5a0e17490d9aee4974109f7788bec8215c463
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-12-14 12:09:26 +0500

    Add timestamp

--- frontend/src/components/chat/MessageBubble.js
+++ frontend/src/components/chat/MessageBubble.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 export default function MessageBubble(props) {
     let msg = props.message;
+    let isSender = msg.sender.id === props.senderId
     let bubbleClass = (
-        msg.sender.id === props.senderId
+        isSender
             ? "align-self-end bg-dark text-light"
             : "align-self-start bg-primary text-dark");
     let msgDirectionClass = (
-        msg.sender.id === props.senderId
+        isSender
             ? "flex-row-reverse"
             : "flex-row");
+    let timeString = (new Date(msg.time).toLocaleTimeString() + " on "
+        + new Date(msg.time).toLocaleDateString())
     return (
         <div className={`p-2 m-2 ${bubbleClass} card`}>
             <div className={`${msgDirectionClass} d-flex align-items-center`}>
@@ -18,7 +21,10 @@ export default function MessageBubble(props) {
                     className="rounded mx-1"
                 />
                 <div className="mx-1">
-                    <small>{msg.sender.first_name} {msg.sender.last_name}</small><br />
+                    <div className={`${isSender ? "flex-row-reverse" : ""} d-flex justify-content-between`}>
+                        <small><b>{msg.sender.first_name} {msg.sender.last_name}</b></small>
+                        <small className="text-muted">{timeString}</small>
+                    </div>
                     <span>{msg.text}</span>
                 </div>
             </div>
