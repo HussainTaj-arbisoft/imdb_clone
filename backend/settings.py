75ba8129f3da4f1d2fb7969b4fc0f8223b6b078e
HussainTaj <hussaintaj@a006-00220>
2020-12-30 18:50:26 +0500

    Update Django DB config

--- backend/settings.py
+++ backend/settings.py
@@ -113,15 +113,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
-    #     "NAME": "imdb",
-    #     "USER": "imdb",
-    #     "PASSWORD": "imdb",
-    # },
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
         "NAME": "imdb",
+        "USER": "root",
+        "PASSWORD": "secret",
+        "HOST": "db",
+        "PORT": "3306"
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
